a7daf4835ce18ff6d414a10161e0462874f23655
Lord-Einarr <lord.einarr@gmail.com>
2017-05-21 15:54:48 +1200

    Added blank website template
    Have added the basic blank website template that i usually use to start
    most sites

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+	$page['title'] = 'ENTER PAGE TITLE HERE';
+    require 'partials/header.php';
+    require 'partials/navigation.php';
+?>
+<div class="container page-wrapper">
+	
+</div>	
+<?php
+    require 'partials/footer.php';
+?>
---
+++ partials/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+        
+        <!-- MET TAGS AND SEO -->
+    	<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+    	<!-- PAGE TITLE -->
+        <title><?= (!empty($page['title'])) ? $page['title'] : 'Default Page' ?></title>
+
+    	<!-- INTERNAL STYLE SHEET -->
+        <link  rel="stylesheet" href="css/app.css">
+        <link rel="stylesheet" type="text/css" href="css/custom.css">
+
+	</head>
+
+    <body>
